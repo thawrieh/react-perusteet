3bc08d45bdbe14aecd5079b19ff44f20ce57284d
s2300283 <s2300283@edubc.fi>
2023-10-14 19:51:09 +0300

    task1.3

--- 02-soveltavat-tehtavat/part-1/ kurssitiedot/src/App.jsx
+++ 02-soveltavat-tehtavat/part-1/ kurssitiedot/src/App.jsx
@@ -1,53 +1,25 @@
-
-const Header = () => {
-  const course = "Half Stack application development";
-  return <h1>{course}</h1>;
-};
-const Part= (props) => {
-  console.log(props);
-  return (
-    <div>
-        {props.part} {props.exercise}
-   </div>
-  );
-};
-const Content = ()=> {
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-  return (
-    <div>
-      <Part part={part1} exercise={exercises1}/>
-      <Part part={part2} exercise={exercises2} />
-      <Part part={part3} exercise={exercises3}/>
-    </div>
-  );
-};
-const Total = (props) => {
-  return (
-    <div>
-     Number of exercises {props.num}
-    </div>
-  );
-};
-
 const App = () => {
-  // const-määrittelyt
-  const course = "Half Stack application development";
-  const exercises1 = 10;
-  const exercises2 = 7;
-  const exercises3 = 14;
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   return (
     <div>
-      <Header course={course} />
-      <Content/>
-    <Total num={exercises1+exercises2+exercises3
-    } />
+    <h1>{course}</h1>
+    <p>{part1.name} {part1.exercises}</p>
+    <p>{part2.name} {part2.exercises}</p>
+    <p>{part3.name} {part3.exercises}</p>
+    <p>Number of exercises {part1.exercises+part2.exercises+part3.exercises}</p>
     </div>
   );
 };
-
 export default App;
