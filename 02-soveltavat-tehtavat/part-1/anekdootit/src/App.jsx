087840ff9b66788cae625832f1cf489680b2c7be
s2300283 <s2300283@edubc.fi>
2023-10-14 20:39:58 +0300

    task1.13

--- 02-soveltavat-tehtavat/part-1/anekdootit/src/App.jsx
+++ 02-soveltavat-tehtavat/part-1/anekdootit/src/App.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
-    'The only way to go fast, is to go well.'
+  const initialAnecdotes = [
+    { text: 'If it hurts, do it more often.', votes: 0 },
+    { text: 'Adding manpower to a late software project makes it later!', votes: 0 },
+    { text:'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', votes: 0 },{ text:
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', votes: 0 },{ text:
+    'Premature optimization is the root of all evil.', votes: 0 }
+    // more
   ];
+
+  const [anecdotes, setAnecdotes] = useState([...initialAnecdotes]);
 
   const [selected, setSelected] = useState(0);
 
@@ -19,10 +19,20 @@ const App = () => {
     setSelected(randomIndex);
   };
 
+  const handleVoteClick = () => {
+    const updatedAnecdotes = [...anecdotes];
+    updatedAnecdotes[selected] = { ...updatedAnecdotes[selected], votes: updatedAnecdotes[selected].votes + 1 };
+    setAnecdotes(updatedAnecdotes);
+  };
+
+
   return (
     <div>
-      <div>{anecdotes[selected]}</div>
-      <p></p>
+      <div>
+        <p>{anecdotes[selected].text}</p>
+        <p>has {anecdotes[selected].votes} votes</p>
+      </div>
+      <button onClick={handleVoteClick}>Vote</button>
       <button onClick={handleRandomClick}>next anecdote</button>
     </div>
   );
